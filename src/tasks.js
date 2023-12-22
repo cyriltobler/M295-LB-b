560e13e0b21c385498e37a597d74a5407b56f9a1
Cyril Tobler <cyriltobler@icloud.com>
2023-12-22 08:40:42 +0000

    feat: Created post /tasks

--- src/tasks.js
+++ src/tasks.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const { randomUUID } = require('node:crypto');
 
 const tasks = [];
 
@@ -9,6 +9,24 @@ router.get('/', (req, res) => {
     res.json(tasks);
 });
 
+router.post('/', (req, res) => {
+    const {title, author} = req.body;
+    if(!title || !author){
+        return res.status(422).json({error: "Title and author are required"});
+    };
+
+    //create a new task
+    const newTask = {
+        id:  randomUUID(),
+        title: title,
+        author: author,
+        created_at: new Date(),
+        done_at: undefined
+    };
+    tasks.push(newTask);
+
+    res.status(201).json(newTask);
+});
 
 
 module.exports = router;

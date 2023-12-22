a0222f409528cae638048013f67e6fe9ea121cfd
Cyril Tobler <cyriltobler@icloud.com>
2023-12-22 12:22:25 +0000

    feat: Created delete /logout

--- src/auth.js
+++ src/auth.js
@@ -9,7 +9,7 @@ router.post('/login', (req, res) => {
     const { email, password } = req.body;
 
     if(!email || !password){
-        return res.status(422).json({error: "Email and password are required"});
+        return res.status(422).json({error: 'Email and password are required'});
     };
 
     if(password === credentials.password){
@@ -17,7 +17,7 @@ router.post('/login', (req, res) => {
 
 		return res.status(201).json({ email: req.session.email });
     }else{
-        return res.status(401).send({error: "Wrong email or password"});
+        return res.status(401).send({error: 'Wrong email or password'});
     };
 });
 
@@ -28,5 +28,14 @@ router.get('/verify', (req, res) => {
     return res.status(401).json({ error: 'Not logged in' });
 });
 
+router.delete('/logout',(req, res) => {
+    if(req.session.email){
+        req.session.email = null
+        
+		return res.status(204).send();
+	}
+
+    return res.status(401).json({ error: 'Not logged in' })
+})
 
 module.exports = router;

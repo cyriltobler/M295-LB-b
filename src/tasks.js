03aee76b7ea7bdc5822f998d93369889bf43fefb
Cyril Tobler <cyriltobler@icloud.com>
2023-12-22 09:17:16 +0000

    feat: Created get /tasks/{id}

--- src/tasks.js
+++ src/tasks.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { randomUUID } = require('node:crypto');
+const { runInNewContext } = require('node:vm');
 
-const tasks = [];
+const tasks = [
+    {
+        id: "1",
+        title: "Bahnhof",
+        author: "Cyril",
+        created_at: "2023-12-22T08:39:34.691Z"
+    }
+];
 
 
 router.get('/', (req, res) => {
@@ -28,5 +36,14 @@ router.post('/', (req, res) => {
     res.status(201).json(newTask);
 });
 
+router.get('/:id', (req, res) => {
+    const task = tasks.find((task) => task.id === req.params.id)
+
+    if(!task){
+        res.status(404).json({error: "ID not found"});
+    }
+
+    res.json(task)
+});
 
 module.exports = router;
